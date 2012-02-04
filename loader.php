adf2f8f2d5986c6ded24cd4629c4c72c67743df9
r-a-y <r-a-y@gmx.com>
2012-02-04 13:26:21 -0800

    Add BP 1.5 compatibility. Add some new template tags and filters.

--- loader.php
+++ loader.php
@@ -7,12 +7,12 @@ Description: Member comment feeds
 Version: 1.0
 */
 
+// Exit if accessed directly
+if ( !defined( 'ABSPATH' ) ) exit;
+
 function bplf_loader() {
 	require_once( dirname(__FILE__) . '/bp-lotsa-feeds.php' );
 }
-add_action( 'bp_loaded', 'bplf_loader' );
-
-
-
+add_action( 'bp_include', 'bplf_loader' );
 
 ?>
